7e218264928945f00570c79966adc82f8283c5f2
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-08-30 12:51:12 +0200

    [add] feat header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,24 @@
 const Header = () => {
   return (
-    <div>
-
-    </div>
+    <header>
+      <div>
+        <img src="/img/dc-logo.png" alt="Logo" />
+      </div>
+      <div>
+        <ul id='links'>
+          <li>CHARACTERS</li>
+          <li>COMICS</li>
+          <li>MOVIES</li>
+          <li>TV</li>
+          <li>GAMES</li>
+          <li>COLLECTIBLES</li>
+          <li>VIDEOS</li>
+          <li>FANS</li>
+          <li>NEWS</li>
+          <li>SHOP</li>
+        </ul>
+      </div>
+    </header>
   )
 }
 
