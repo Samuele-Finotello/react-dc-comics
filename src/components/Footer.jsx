0afd94cc05ca818a75eaccb5118fccfda4147f86
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-08-30 19:44:58 +0200

    [add] feat footer #marketplace

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,37 @@
 const Footer = () => {
   return (
-    <div>
+    <footer>
+      <div id="marketplace">
+        <div className="space-around container">
+          <div>
+            <img src="/img/buy-comics-digital-comics.png" alt="Digital Comics" />
+            <span>DIGITAL COMICS</span>
+          </div>
+          <div>
+            <img src="/img/buy-comics-merchandise.png" alt="Merchandise" />
+            <span>DC MERCHANDISE</span>
+          </div>
+          <div>
+            <img src="/img/buy-comics-subscriptions.png" alt="Subscriptions" />
+            <span>SUBSCRIPTION</span>
+          </div>
+          <div>
+            <img src="/img/buy-comics-shop-locator.png" alt="Shop Locator" />
+            <span>COMIC SHOP LOCATOR</span>
+          </div>
+          <div>
+            <img src="/img/buy-dc-power-visa.svg" alt="Power Visa" />
+            <span>DC POWER VISA</span>
+          </div>
+        </div>
+      </div>
+      <div id="info">
 
-    </div>
+      </div>
+      <div id="socials">
+
+      </div>
+    </footer>
   )
 }
 
