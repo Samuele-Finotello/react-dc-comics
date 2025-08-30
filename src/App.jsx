971d4f77c4e783cfa2bb9666d5794744aa528907
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-08-30 12:29:17 +0200

    [add] creation of components Header, Main, Footer and importation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
-function App() {
+//importazione components
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  )
 }
 
 export default App
