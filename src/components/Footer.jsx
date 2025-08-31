8ca6eb92e1f00fa8a88f49842f9ac1bff738d732
Samuele-Finotello <samuelefinotello.147@gmail.com>
2025-08-31 14:09:07 +0200

    [add] feat bonus Marketplace, end exercise 25/08/2025

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,30 +1,9 @@
+import Marketplace from "./Marketplace"
+
 const Footer = () => {
   return (
     <footer>
-      <div id="marketplace">
-        <div className="space-around container">
-          <div>
-            <img src="/img/buy-comics-digital-comics.png" alt="Digital Comics" />
-            <span>DIGITAL COMICS</span>
-          </div>
-          <div>
-            <img src="/img/buy-comics-merchandise.png" alt="Merchandise" />
-            <span>DC MERCHANDISE</span>
-          </div>
-          <div>
-            <img src="/img/buy-comics-subscriptions.png" alt="Subscriptions" />
-            <span>SUBSCRIPTION</span>
-          </div>
-          <div>
-            <img src="/img/buy-comics-shop-locator.png" alt="Shop Locator" />
-            <span>COMIC SHOP LOCATOR</span>
-          </div>
-          <div>
-            <img id="visa" src="/img/buy-dc-power-visa.svg" alt="Power Visa" />
-            <span>DC POWER VISA</span>
-          </div>
-        </div>
-      </div>
+      <Marketplace />
       <div id="info">
         <div className="space-between container">
           <div id="lists">
---
+++ src/components/Marketplace.jsx
@@ -0,0 +1,30 @@
+const Marketplace = () => {
+  return (
+    <div id="marketplace">
+      <div className="space-around container">
+        <div>
+          <img src="/img/buy-comics-digital-comics.png" alt="Digital Comics" />
+          <span>DIGITAL COMICS</span>
+        </div>
+        <div>
+          <img src="/img/buy-comics-merchandise.png" alt="Merchandise" />
+          <span>DC MERCHANDISE</span>
+        </div>
+        <div>
+          <img src="/img/buy-comics-subscriptions.png" alt="Subscriptions" />
+          <span>SUBSCRIPTION</span>
+        </div>
+        <div>
+          <img src="/img/buy-comics-shop-locator.png" alt="Shop Locator" />
+          <span>COMIC SHOP LOCATOR</span>
+        </div>
+        <div>
+          <img id="visa" src="/img/buy-dc-power-visa.svg" alt="Power Visa" />
+          <span>DC POWER VISA</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Marketplace
